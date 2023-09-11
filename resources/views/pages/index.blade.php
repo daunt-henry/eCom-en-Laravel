93fa62d66928fc47bda668a3f0e400d0e52a89d0
Henry <daunthenry462@gmail.com>
2023-09-11 22:56:37 +0530

    header, footer and login page done

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'Project')}}</title>
+
+    </head>
+    <h1>Welcome to laravel for beginners</h1>
+    <p>This is a laravel turtorial series</p>
+    <body>
+
+    </body>
+</html>
